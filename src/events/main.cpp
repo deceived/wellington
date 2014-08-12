d19e01fe17b71fbfcff3b2fab1af5c8282896d6d
john ratcliffe <macro32@users.noreply.github.com>
2014-08-12 21:47:03 +0100

    built and used boost 1.56 to add basic command processing.

--- src/events/main.cpp
+++ src/events/main.cpp
@@ -1,15 +1,22 @@
 #include <iostream>
 #include <string>
+#include <vector>
+
+#include <boost/algorithm/string.hpp>
+#include <boost/smart_ptr.hpp>
+
+using namespace boost;
 
 class ICommand
 {
 
 public:
 
-    virtual ~ICommand();
+    virtual ~ICommand()
+    {}
  
     virtual void Execute() = 0;
-
+    virtual std::string ToString() = 0;
 };
 
 class MoveCommand : public ICommand
@@ -17,11 +24,22 @@ class MoveCommand : public ICommand
 
 public:
 
-    virtual void Execute();
+    MoveCommand( const std::string& command )
+        : command_( command )
+    {}
 
+    virtual void Execute()
+    {}
+
+    virtual std::string ToString()
+    {
+        return command_;
+    }
+    
 private:
 
 // state
+    std::string command_;
 
 };
 
@@ -30,11 +48,22 @@ class EngagementCommand : public ICommand
 
 public:
 
-    virtual void Execute();
+    EngagementCommand( const std::string& command )
+        : command_( command )
+    {}
+
+    virtual void Execute()
+    {}
+
+    virtual std::string ToString()
+    {
+        return command_;
+    }
 
 private:
 
 // state
+    std::string command_;
 
 };
 
@@ -43,18 +72,56 @@ class ExitCommand : public ICommand
 
 public:
 
-    virtual void Execute();
+    ExitCommand( const std::string& command )
+        : command_( command )
+    {}
+
+    virtual void Execute()
+    {}
+
+    virtual std::string ToString()
+    {
+        return command_;
+    }
 
 private:
 
 // state
+    std::string command_;
 
 };
 
-
-ICommand*   ParseCommand( const std::string& line )
+class CommandFactory
 {
-    return 0;
+
+public:
+
+    static shared_ptr<ICommand> Create( const std::string name )
+    {
+        if( name == "move" )
+        {
+            return make_shared<MoveCommand>( name ); 
+        }
+        if( name == "defend" )
+        {
+            return make_shared<EngagementCommand>( name ); 
+        }
+        if( name == "attack" )
+        {
+            return make_shared<EngagementCommand>( name ); 
+        }
+        return shared_ptr<ICommand>();
+    }
+ 
+};
+
+shared_ptr<ICommand>   ParseCommand( const std::string& line )
+{
+    std::vector< std::string > split_vector;
+
+    split(  split_vector, line, is_any_of( " " ) );
+    
+    return CommandFactory::Create( split_vector[0] );
 }
 
 int main( int argc, char** argv )
@@ -67,9 +134,12 @@ int main( int argc, char** argv )
         std::cout << ". ";
         std::getline( std::cin, line );
 
-        ICommand* command = ParseCommand( line );
+        shared_ptr<ICommand> command = ParseCommand( line );
 
-        std::cout << line << std::endl;
+        if( command != 0 )
+        {
+            std::cout << command->ToString() << std::endl;
+        }
 
     } while( line != "done" );    
 
