f024b2885fa2be75a3d2f9270fbfb75a129c2827
john ratcliffe <macro32@users.noreply.github.com>
2014-08-27 19:38:53 +0100

    added map and units skeletons.

---
+++ src/events/map.hpp
@@ -0,0 +1,14 @@
+#ifndef MAP_HPP
+#define MAP_HPP
+
+class   Map
+{
+
+public:
+
+private:
+
+};
+
+#endif
+
---
+++ src/events/units.hpp
@@ -0,0 +1,174 @@
+#ifndef UNITS_HPP
+#define UNITS_HPP
+
+class   IUnit
+{
+
+public:
+
+};
+
+class   Infantry    : public IUnit
+{
+
+public:
+
+private:
+
+};
+
+class   Riflemen    : public Infantry
+{
+
+public:
+
+private:
+
+};
+
+class   Light : public Infantry
+{
+
+public:
+
+private:
+
+};
+
+class   Line : public Infantry 
+{
+
+public:
+
+private:
+
+};
+
+
+
+
+
+class   Artillery   : public IUnit
+{
+
+public:
+
+private:
+
+};
+
+class   Rocket : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+class   Seige : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+class   HeavyField : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+class   LightField : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+class   HeavyHorse : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+class   LightHorse : public Artillery
+{
+
+public:
+
+private:
+
+};
+
+
+
+
+class   Cavalry     : public IUnit
+{
+
+public:
+
+private:
+
+};
+
+class  Officer : public Cavalry
+{
+
+public:
+
+private:
+
+};
+
+class  Heavy : public Cavalry
+{
+
+public:
+
+private:
+
+};
+
+class  Medium : public Cavalry
+{
+
+public:
+
+private:
+
+};
+
+class  Light : public Cavalry
+{
+
+public:
+
+private:
+
+};
+
+class  Lancers : public Cavalry
+{
+
+public:
+
+private:
+
+};
+
+
+
+#endif
+
