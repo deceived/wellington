6cf275f74625156f5bfb79d186c8a0a597507968
john ratcliffe <macro32@users.noreply.github.com>
2014-08-28 22:00:07 +0100

    added file stream reader.

--- src/events/command.hpp
+++ src/events/command.hpp
@@ -1,6 +1,8 @@
 #ifndef COMMAND_HPP
 #define COMMAND_HPP
 
+#include <iostream>
+#include <fstream>
 #include <vector>
 
 #include <boost/algorithm/string.hpp>
@@ -104,6 +106,45 @@ private:
 
 ***/
 
+
+
+/***
+    RAII implementation reading a file stream.
+***/
+class InputFileStream
+{
+
+public:
+
+    FileStream()
+        : stream_( fileName )
+    {}
+
+    ~FileStream()
+    {
+        stream_.close();
+    }
+
+    std::string ReadLine()
+    {
+        std::string line;
+        std::getline( stream_, line );
+        return line;
+    }
+
+    bool Eof()
+    {
+        return stream_.eof();
+    }
+ 
+private:
+
+    std::ifstream   stream_;
+
+};
+
+
+
 class LoadRulesCommand : public ICommand
 {
 
@@ -157,6 +198,7 @@ private:
 
     void ParseCommands()
     {
+        
     }
 
     std::vector< std::string > commands_;
