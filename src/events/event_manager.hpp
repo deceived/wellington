4edd62ce8cc91168e7296491ca2440fdd7669eae
john ratcliffe <macro32@users.noreply.github.com>
2014-09-02 22:05:31 +0100

    first cut at event manager.

--- src/events/event_manager.hpp
+++ src/events/event_manager.hpp
@@ -5,12 +5,13 @@
 #include <map>
 
 #include <boost/smart_ptr.hpp>
+#include <boost/utility.hpp>
 
 #include "FastDelegate.h"
 #include "event.hpp"
 
 
-class EventManager
+class EventManager : public boost::noncopyable
 {
 
 public:
@@ -19,22 +20,16 @@ public:
     typedef std::map<IEvent::EventType, EventListenerList> EventListenerMap;
     typedef std::list<IEvent::IEventPtr> EventQueue;
 
-	explicit EventManager( const char* pName );
-	virtual ~EventManager();
-
-    virtual bool AddListener( const IEvent::EventListenerDelegate& eventDelegate, const IEvent::EventType type);
-	virtual bool RemoveListener( const IEvent::EventListenerDelegate& eventDelegate, const IEvent::EventType type);
+    virtual bool AddListener( const IEvent::EventListenerDelegate eventDelegate, const IEvent::EventType type);
+	virtual bool RemoveListener( const IEvent::EventListenerDelegate eventDelegate, const IEvent::EventType type);
 
 	virtual bool TriggerEvent( const IEvent::IEventPtr event ) const;
 
 	virtual bool QueueEvent( const IEvent::IEventPtr event );
-	virtual bool ThreadSafeQueueEvent( const IEvent::IEventPtr event );
 
 	virtual bool AbortEvent( const IEvent::EventType type, bool allOfType = false);
 
 	virtual bool Update( unsigned long maxMillis );
-
-	static boost::shared_ptr<EventManager> Get(void);
 
 private:
 
