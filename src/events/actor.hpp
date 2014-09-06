04e99596c78a84e117b4529e5316a2fe9701b79b
john ratcliffe <macro32@users.noreply.github.com>
2014-09-06 17:11:52 +0100

    added base actor and test.

--- src/events/actor.hpp
+++ src/events/actor.hpp
@@ -1,16 +1,81 @@
 #ifndef ACTOR_HPP
 #define ACTOR_HPP
 
+#include <map>
+
+#include <boost/property_tree/ptree.hpp>
+
 class   Actor
 {
 
 public:
 
-    typedef unsigned long ActorId;
+    typedef unsigned long Id;
+
+//    typedef std::map< Component::Id, ActorComponent::Ptr > ActorComponents;
+
+    explicit Actor( Id id )
+    {
+        actorId_ = id;
+    }
+
+    ~Actor() {};
+
+    bool Init( boost::property_tree::ptree& data );
+    void PostInit();
+    void Destroy();
+
+    void Update( int deltaMs );
+
+    Actor::Id   GetId()
+    {
+        return actorId_;
+    }
+
+#if 0
+    template< class ComponentType >
+    boost::weak_ptr<ComponentType> GetComponent( Component::Id id )
+    {
+        ActorComponents::iterator it = components_.find( id );
+        if( it != components_.end() )
+        {
+            ActorComponent::Ptr base( it->second );
+            boost::shared_ptr< ComponentType > sub( std::static_ptr_cast<ComponentType>( base ) );
+            boost::weak_ptr<ComponentType > weakSub( sub );
+            return weakSub;
+        }
+        else
+        {
+            return boost::weak_ptr<ComponentType>();
+        }
+    }
+
+    template <class ComponentType>
+    boost::weak_ptr<ComponentType> GetComponent(const std::string& name)
+    {
+		Component::Id id = ActorComponent::GetIdFromName(name);
+        ActorComponents::iterator it = components_.find( id );
+        if( it != components_.end())
+        {
+            ActorComponent::Ptr base( it->second );
+            boost::shared_ptr<ComponentType> sub( std::static_pointer_cast<ComponentType>( base ) );  
+            boost::weak_ptr<ComponentType> weakSub( sub );  
+            return weakSub;  
+        }
+        else
+        {
+            return boost::weak_ptr<ComponentType>();
+        }
+    }
+#endif
 
 private:
 
-    ActorId actorId_;
+//    void AddComponent( ActorComponent::Ptr component );
+
+    Id actorId_;
+
+//    ActorComponents components_;
 
 };
 
