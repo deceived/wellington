9f8eaa96f57d5f3ea4372117e9e96ab7f0c5f5c5
john ratcliffe <macro32@users.noreply.github.com>
2014-09-06 21:08:27 +0100

    extend tests.

--- src/events/test_actor_factory.cpp
+++ src/events/test_actor_factory.cpp
@@ -7,5 +7,11 @@ int main( int argc, char** argv )
 
     ActorFactory    actorFactory;
 
+    Actor::ptr wellington = actorFactory.CreateActor( "wellington" );
+    Actor::ptr line20thFoot = actorFactory.CreateActor( "20thFoot" );
+    Actor::ptr heavyCavalryScotsGreys = actorFactory.CreateActor( "scotgreys" );
+    Actor::ptr rifles92ndRifles = actorFactory.CreateActor( "92ndRifles" );
+    
+
     return 0;
 }
