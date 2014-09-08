acd58aa0d05fd79ea607bdcface9c0d64c2ea3b5
john ratcliffe <macro32@users.noreply.github.com>
2014-09-08 21:28:25 +0100

    got it to compile but not ld :)

--- src/events/build_actor_factory.sh
+++ src/events/build_actor_factory.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ -std=c++11 -o actor_factory test_actor_factory.cpp actor_factory.cpp
+g++ -std=c++11 -o actor_factory test_actor_factory.cpp actor_factory.cpp actor.cpp 
 ./actor_factory
