8315fa71e052e214400e55103726c7b9b989c790
john ratcliffe <macro32@users.noreply.github.com>
2014-09-28 22:25:22 +0100

    small tidy of build script.

--- src/events/build_actor_factory.sh
+++ src/events/build_actor_factory.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+[ -f actor_factory ] && rm actor_factory
 g++ -std=c++11 -o actor_factory test_actor_factory.cpp actor_factory.cpp actor.cpp 
-./actor_factory
+[ -f actor_factory ] && ./actor_factory
