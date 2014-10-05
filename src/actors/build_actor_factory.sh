8141d76de11726d44bd727ac0defc5297b22713b
john ratcliffe <macro32@users.noreply.github.com>
2014-10-05 21:14:58 +0100

    more tidy up. added pugixml and turtle mock c++ test library as 3rd party code.

--- src/actors/build_actor_factory.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-[ -f actor_factory ] && rm actor_factory
-g++ -std=c++11 -o actor_factory test_actor_factory.cpp actor_factory.cpp actor.cpp ai_component.cpp fight_component.cpp identity_component.cpp move_component.cpp order_component.cpp reaction_component.cpp sprite_component.cpp pugixml.cpp
-[ -f actor_factory ] && ./actor_factory
