32d6b1e3de118d27ebd3e71691b1b09a65d8c17d
john ratcliffe <macro32@users.noreply.github.com>
2014-10-06 21:58:17 +0100

    add a simple window.

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -4,13 +4,48 @@
 #include <ncurses.h>
 
 
+WINDOW* create_win( int height, int width, int starty, int startx );
+void destroy_win( WINDOW* local_win );
+
 int main( int argc, char** argv )
 {
+	WINDOW* win;
+	int startx, starty, width, height;
+	int ch;
+
     initscr();
-    printw("hello world.");
+    cbreak();
+
+	height = 20;
+	width = 30;
+	starty = (LINES - height);
+	startx = (COLS - width);
+
     refresh();
+	win = create_win( height, width, starty, startx );
+
     getch();
     endwin();
 
     return 0;
+}
+
+
+WINDOW* create_win( int height, int width, int starty, int startx )
+{	
+    WINDOW* win;
+
+	win = newwin( height, width, starty, startx );
+	box( win, 0 , 0 );
+	wrefresh( win );
+
+	return win;
+}
+
+
+void destroy_win(WINDOW* local_win)
+{	
+	wborder(local_win, ' ', ' ', ' ',' ',' ',' ',' ',' ');
+	wrefresh(local_win);
+	delwin(local_win);
 }
