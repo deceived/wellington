8ef593e0abd5ac50a613ad1715993ea483942a7a
john ratcliffe <macro32@users.noreply.github.com>
2014-10-15 21:13:00 +0100

    add key model basic.

---
+++ src/main/key_model.cpp
@@ -0,0 +1,15 @@
+#include "map_model.hpp"
+
+
+void MapMode::Load( std::string fileName )
+{
+}
+
+size_t MapNode::LineCount()
+{
+}
+
+std::string MapNode::NextLine()
+{
+}
+
---
+++ src/main/key_model.hpp
@@ -0,0 +1,29 @@
+#ifndef KEY_MODEL_HPP
+#define KEY_MODEL_HPP
+
+#include <string>
+
+#include <boost/smart_ptr.hpp>
+
+class KeyModel
+{
+
+public:
+
+	typedef boost::shared_ptr< Key >	key_ptr;
+
+	KeyModel()
+	{}
+
+	void Load( std::string fileName );
+
+	std::string NextLine();
+	size_t LineCount();
+
+private:
+
+	key_ptr		key_;
+};
+
+#endif
+
