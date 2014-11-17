93fa89d2c385c471262bb166936008a3f3d168a6
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-11-17 20:33:15 +0000

    keep on learning boost logging

--- src/test/log/test_log.cpp
+++ src/test/log/test_log.cpp
@@ -1,6 +1,29 @@
 #include <iostream>
 
+#include <boost/log/core.hpp>
 #include <boost/log/trivial.hpp>
+#include <boost/log/expressions.hpp>
+
+void init()
+{
+    boost::log::core::get()->set_filter
+    (
+        boost::log::trivial::severity >= boost::log::trivial::info
+    );
+}
+
+
+void boost_trivial_init()
+{
+    init();
+
+    BOOST_LOG_TRIVIAL( trace ) << "A trace message";
+    BOOST_LOG_TRIVIAL( debug ) << "A debug message";
+    BOOST_LOG_TRIVIAL( info ) << "A info message";
+    BOOST_LOG_TRIVIAL( warning ) << "A warning message";
+    BOOST_LOG_TRIVIAL( error ) << "A error message";
+    BOOST_LOG_TRIVIAL( fatal ) << "A fatal message";
+}
 
 void boost_trivial()
 {
@@ -12,10 +35,15 @@ void boost_trivial()
     BOOST_LOG_TRIVIAL( fatal ) << "A fatal message";
 }
 
-
 int main( int argc, char** argv )
 {
     std::cout << "test log..." << std::endl;
 
     boost_trivial();
+
+    std::cout << "\n\n";
+
+    boost_trivial_init();
+
+    return 0;
 }
