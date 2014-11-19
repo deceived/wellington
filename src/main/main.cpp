e2831fa766e804ae2ed394c4482e9a48e84b8326
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-11-19 21:24:04 +0000

    remove unwanted log line

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -7,8 +7,6 @@ namespace src = boost::log::sources;
 
 #include "stdscr_map_controller.hpp"
 
-//BOOST_LOG_INLINE_GLOBAL_LOGGER_DEFAULT( the_log, src::logger_mt )
-
 namespace po = boost::program_options;
 po::options_description desc("Allowed options");
 po::variables_map vm;
