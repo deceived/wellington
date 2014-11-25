1e6d292065fca776cfa59f6a4f4c6c8cb89d698a
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-11-25 20:53:38 +0000

    more tracing. can never have too much.. ;)

--- src/actors/actor_factory.cpp
+++ src/actors/actor_factory.cpp
@@ -56,6 +56,8 @@ ActorPtr    ActorFactory::CreateActor( const std::string& actorResource )
 ActorComponentPtr ActorFactory::CreateComponent( pugi::xml_node  component )
 {
     logger.Severity( severity_level::info, __PRETTY_FUNCTION__ );
+    logger.Severitty( severity_level::debug, component.attribute( "name" ).value() );
+
     ActorComponentPtr   cp( componentFactory_.Create( ActorComponent::GetIdFromName( component.attribute("name").value() ) ) );
     if( cp )
     {
