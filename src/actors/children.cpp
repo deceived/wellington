1c81cf92938ef5b1a7c96f1333c62cfb5980fa30
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-11-28 21:42:54 +0000

    remove an oops file commit

--- src/actors/children.cpp
+++
@@ -1,5 +0,0 @@
-    for( pugi::xml_node child: data.children() )
-    {
-        logger.Severity( severity_level::debug, child.name() );
-        logger.Severity( severity_level::debug, child.child_value() );
-    }
