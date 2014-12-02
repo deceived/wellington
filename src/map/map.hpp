3dca9439c7bd3522b3c69f2f0c2ba3427b10fdcb
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-12-02 22:23:02 +0000

    start a map class.

--- src/map/map.hpp
+++ src/map/map.hpp
@@ -1,13 +1,27 @@
 #ifndef MAP_HPP
 #define MAP_HPP
 
+#include <array>
+
+#include "tile.hpp"
+
 class Map
 {
 
 public:
 
+    Map( int x, int y  )
+	:  x_( x ),
+	   y_( y )
+	{}
+	 
 private:
 
+    int x_;
+    int y_;
+
+    std::array< std::array< Tile, x_ >, y_ > map_;
+ 
 };
 
 
