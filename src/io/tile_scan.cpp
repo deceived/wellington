4592061e9c767a2d73435041078487b7d0127c09
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-12-06 16:53:16 +0000

    converge scanner and data. :)

--- src/io/tile_scan.cpp
+++ src/io/tile_scan.cpp
@@ -13,3 +13,56 @@ int TileScanner::Class( int c )
     return c;
 }
 
+int TileScanner::Input()
+{
+	if( index_ < line_.size() )
+	{
+		return line_[ index_++ ];
+	}
+    if( !input.eof() )
+    {
+		getline( input_, line_ );
+		boost::to_lower( line_ );
+		index_ = 0;
+		return line_[ index_++ ];
+    }
+}
+
+void TileScanner::Unput()
+{
+	--input_;
+}
+
+int TileScanner::NextSymbol()
+{
+	int c;
+	c = Input();
+	while( c != EOFSY )
+	{
+		switch( Class( c ) )
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+				break;
+			case ALPHA:
+				break;
+		    case DIGIT:
+				break;
+			case '{':
+				break;
+			case '}':
+				break;
+			case '[':
+				break;
+			case ']':
+				break;
+			case '(':
+				break;
+			case ')':
+				break;
+			case '=':
+				break;
+		}
+	}
+}
--- src/io/tile_scan.hpp
+++ src/io/tile_scan.hpp
@@ -1,10 +1,16 @@
 #ifndef SCAN_HPP
 #define SCAN_HPP
 
+#include <iostream>
 #include <fstream>
+#include <string>
+#include <map>
+
+#include <boost/algorithm/string.hpp>
 
 typedef enum 
 {
+	UNKNOWNSY,
 	ENUMSY,
 	TERRAINSY,
 	COVERSY,
@@ -33,6 +39,51 @@ typedef enum
 } symbolclass;
 
 
+class ReservedWords
+{
+
+public:
+
+	typedef std::map< std::string, symbolclass > reserved_type;
+
+	ReservedWords()
+	{
+		reserved_["enum"] = ENUMSY;
+		reserved_["terrain"] = TERRAINSY;
+		reserved_["cover"] = COVERSY;
+		reserved_["flat"] = FLATSY;
+		reserved_["hill"] = HILLSY;
+		reserved_["mountain"] = MOUNTAINSY;
+		reserved_["lake"] = LAKESY;
+		reserved_["stream"] = STREAMSY;
+		reserved_["river"] = RIVERSY;
+		reserved_["grass"] = GRASSSY;
+		reserved_["marsh"] = MARSHSY;
+		reserved_["wood"] = WOODSY;
+		reserved_["forest"] = FORESTSY;
+		reserved_["wall"] = WALLSY;
+		reserved_["building"] = BUILDINGSY;
+	}
+
+	bool Exists( const std::string& key )
+	{
+		return 1 == reserved_.count( key );
+	}
+
+    symbolclass Get( const std::string& key )
+	{
+		if( Exists( key ) )
+		{
+			return reserved_[ key ];
+		}
+		return UNKNOWNSY;
+	}
+
+private:
+
+	reserved_type	reserved_;
+
+};
 
 class TileScanner
 {
@@ -40,15 +91,32 @@ class TileScanner
 public:
 
 	TileScanner( const std::string& fileName )
-		: fileName_( fileName )
-	{}
+		: index_( 0 ),
+		  fileName_( fileName )
+	{
+		input_.open( fileName );
+		getline( input_, line_ );
+		boost::to_lower( line_ );
+	}
+
+	TileScanner()
+	{
+		input_.close();
+	}
+
+	int Input();
+	void Unput();
 
 private:
 
 	int Class( int c );
+	int index_;
+	std::string line_;
 
 	std::string fileName_;
 	std::ifstream input_;
+
+	ReservedWords	reserved_;
 
 };
 
