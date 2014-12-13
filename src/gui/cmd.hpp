8d39c8dd2e5611a7d0f782206aa52b3732debd7d
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-12-13 14:19:25 +0000

    simplify things down

---
+++ src/gui/cmd.hpp
@@ -0,0 +1,26 @@
+#ifndef CMD_HPP
+#define CMD_HPP
+
+#include <ncurses.h>
+#include <string>
+
+#include <boost/smart_ptr.hpp>
+
+class Command
+{
+
+public:
+
+	typedef boost::shared_ptr< std::string > line_ptr;
+
+	CmdModel::line_ptr  GetPrompt();
+	void Put( unsigned int row, unsigned int col, CmdView::line_ptr line );
+	void ClearLine( unsigned int row, unsigned int col, unsigned int length );
+	line_ptr Read( unsigned int row, unsigned int col );
+	line_ptr Read();
+
+private:
+
+};
+
+#endif
