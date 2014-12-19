a2c0414b2520b2e453ddef852584a4a207e12b49
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2014-12-19 21:20:38 +0000

    gui library builds

--- src/gui/stdscr_view.cpp
+++ src/gui/stdscr_view.cpp
@@ -1,9 +1,7 @@
 #include "stdscr_view.hpp"
 #include <iostream>
 StdScrView::StdScrView()
-	: 	mapView_( boost::make_shared< MapView >() ),
-		keyView_( boost::make_shared< KeyView >() ),
-		cmdView_( boost::make_shared< CmdView >() ),
+	:
 		rows_( LINES ),
 		columns_( COLS )
 {
@@ -36,20 +34,12 @@ void StdScrView::DisplayLine( unsigned int row, unsigned int column, std::string
 	mvprintw( row, column, line.c_str() );
 }
 
-void StdScrView::ClearLine( unsigned int row, unsigned int column, int length )
-{
-	for(int count = 0; count < length; ++count)
-	{
-		mvaddch( row, column, ' ' );
-	} 
-}
-
 void StdScrView::Put( unsigned int row, unsigned int col, line_ptr line )
 {
 	mvprintw( row, col, (*line).c_str() );
 }
 
-line_ptr StdScrView::Read( unsigned int row, unsigned int col )
+StdScrView::line_ptr StdScrView::Read( unsigned int row, unsigned int col )
 {
 	char cmd[ 128 ];
 
@@ -59,7 +49,7 @@ line_ptr StdScrView::Read( unsigned int row, unsigned int col )
 	return boost::make_shared<std::string>( cmd );
 }
 
-line_ptr StdScrView::Read()
+StdScrView::line_ptr StdScrView::Read()
 {
 	char cmd[ 128 ];
 
