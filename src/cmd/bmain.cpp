b8050f45dd93a5a1a468d2a8331780359dba436b
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-01-29 18:51:59 +0000

    put the parser with an example order

--- src/cmd/bmain.cpp
+++ src/cmd/bmain.cpp
@@ -9,9 +9,13 @@ int main( int argc, char** argv )
 
 	std::cout << "starting...." << std::endl;
 
+	std::string move_1( "MOVE TO THE NW." );
+
 	Order order;
 
 	Driver* driver = new Driver( order );
+
+	driver->ParseString( move_1, "move" );
 	
 	return 0;
 }
