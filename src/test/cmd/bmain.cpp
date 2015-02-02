77b9ed83653293a6fe038574823b38ce66e35bea
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-02-02 21:30:32 +0000

    fix up the test driver program so it runs...:$

--- src/test/cmd/bmain.cpp
+++ src/test/cmd/bmain.cpp
@@ -12,40 +12,40 @@ int main( int argc, char** argv )
 	OrderContext orders;
 	Driver driver( orders );
 
-	if( argc > 1 )
+	for( int i = 1; i < argc; ++i )
 	{
-		for( int i = argc; i < argc; ++i )
+		std::cout << "argument loop\n";
+		if( argv[i] == std::string( "-p" ) )
 		{
-			if( argv[i] == std::string( "-p" ) )
+			std::cout << "trace parsing = true" << std::endl;
+			driver.traceParsing_ = true;
+		}
+		else if( argv[i] == std::string( "-s" ) )
+		{
+			std::cout << "trace scanning = true" << std::endl;
+			driver.traceScanning_ = true;
+		}
+		else
+		{
+			std::fstream infile( argv[i] );
+			if( !infile.good() )
 			{
-				driver.traceParsing_ = true;
-			}
-			else if( argv[i] == std::string( "-s" ) )
-			{
-				driver.traceScanning_ = true;
-			}
-			else
-			{
-				std::fstream infile( argv[i] );
-				if( !infile.good() )
-				{
-					std::cout << "cannot open input file : [" << argv[i] << "]" << std::endl;
-					return 0;
-				}
-				orders.Clear();
-				bool result = driver.ParseStream( infile, argv[i] );
-				if ( result )
-				{
-					std::cout << "orders: " << std::endl;
-					for( unsigned int o = 0; o < driver.orders_.orders_.size(); ++o )
-					{
-						driver.orders_.orders_[o]->Print( std::cout );
-					}
-				}	
+				std::cout << "cannot open input file : [" << argv[i] << "]" << std::endl;
 				return 0;
 			}
-		}	
-	}
+			orders.Clear();
+			bool result = driver.ParseStream( infile, argv[i] );
+			if ( result )
+			{
+				std::cout << "orders: " << std::endl;
+				for( unsigned int o = 0; o < driver.orders_.orders_.size(); ++o )
+				{
+					driver.orders_.orders_[o]->Print( std::cout );
+				}
+			}	
+			return 0;
+		}
+	}	
 
 	do
 	{
