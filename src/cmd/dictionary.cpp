167bcf519e8d7d7cbf4f210b3b21c9eee5f451cd
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-02-12 21:45:16 +0000

    fixing up the entries for the dictionary

--- src/cmd/dictionary.cpp
+++ src/cmd/dictionary.cpp
@@ -4,13 +4,13 @@
 
 Dictionary::Dictionary()
 {
-	dictionary_[ "attack" ] = *new Entry( "attack", Entry::Verb, Entry::Attack, Entry::Strong );
-	dictionary_[ "move"   ] = *new Entry( "move", Entry::Verb, Entry::Neutral, Entry::Neutral );
-	dictionary_[ "defend" ] = *new Entry( "defend", Entry::Verb, Entry::Defend, Entry::Strong );
-	dictionary_[ "support"] = *new Entry( "support", Entry::Verb, Entry::Attack, Entry::Neutral );
-	dictionary_[ "advance"] = *new Entry( "advance", Entry::Verb, Entry::Attack, Entry::Neutral );
-	dictionary_[ "occupy" ] = *new Entry( "occupy", Entry::Verb, Entry::Neutral, Entry::Weak );
-	dictionary_[ "hold"   ] = *new Entry( "hold", Entry::Verb, Entry::Defend, Entry::Neutral );
+	dictionary_[ "attack" ] = new Entry( std::string( "attack" ), Verb, Attack, Strong );
+	dictionary_[ "move"   ] = new Entry( std::string( "move" ), Verb, Neutral, Neutral );
+	dictionary_[ "defend" ] = new Entry( std::string( "defend" ), Verb, Defend, Strong );
+	dictionary_[ "support"] = new Entry( std::string( "support" ), Verb, Attack, Neutral );
+	dictionary_[ "advance"] = new Entry( std::string( "advance" ), Verb, Attack, Neutral );
+	dictionary_[ "occupy" ] = new Entry( std::string( "occupy" ), Verb, Neutral, Weak );
+	dictionary_[ "hold"   ] = new Entry( std::string( "hold" ), Verb, Defend, Neutral );
 }
 
 Entry Dictionary::Get( std::string )
