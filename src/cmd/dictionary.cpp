56366d59d722ceb40a14d1854a15ec3dfdc24e50
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-02-12 19:40:28 +0000

    add details for the first group of order words

--- src/cmd/dictionary.cpp
+++ src/cmd/dictionary.cpp
@@ -4,13 +4,13 @@
 
 Dictionary::Dictionary()
 {
-	dictionary_[ "attack" ] = *new Entry;
-	dictionary_[ "move"   ] = *new Entry;
-	dictionary_[ "defend" ] = *new Entry;
-	dictionary_[ "support"] = *new Entry;
-	dictionary_[ "advance"] = *new Entry;
-	dictionary_[ "occupy" ] = *new Entry;
-	dictionary_[ "hold"   ] = *new Entry;
+	dictionary_[ "attack" ] = *new Entry( "attack", Entry::Verb, Entry::Attack, Entry::Strong );
+	dictionary_[ "move"   ] = *new Entry( "move", Entry::Verb, Entry::Neutral, Entry::Neutral );
+	dictionary_[ "defend" ] = *new Entry( "defend", Entry::Verb, Entry::Defend, Entry::Strong );
+	dictionary_[ "support"] = *new Entry( "support", Entry::Verb, Entry::Attack, Entry::Neutral );
+	dictionary_[ "advance"] = *new Entry( "advance", Entry::Verb, Entry::Attack, Entry::Neutral );
+	dictionary_[ "occupy" ] = *new Entry( "occupy", Entry::Verb, Entry::Neutral, Entry::Weak );
+	dictionary_[ "hold"   ] = *new Entry( "hold", Entry::Verb, Entry::Defend, Entry::Neutral );
 }
 
 Entry Dictionary::Get( std::string )
--- src/cmd/dictionary.hpp
+++ src/cmd/dictionary.hpp
@@ -4,9 +4,39 @@
 #include <string>
 #include <map>
 
+
+
 struct Entry
 {
+	static const unsigned int Verb = 0x1;
+	static const unsigned int Noun = 0x2;
+
+	static const unsigned int Neutral = 0x1;
+	static const unsigned int Defend = 0x2;
+	static const unsigned int Attack = 0x4;
+
+	static const unsigned int Strong = 5;
+	static const unsigned int Neutral = 3;
+	static const unsigned int Weak = 1;
+
+	Entry( 	const std::string& value, 
+			unsigned int speech,
+			unsigned int action,
+			unsigned int strength )
+		: 	value_( value ),
+			speech_( speech ),
+			action_( action ),
+			strength_( strength )
+	{}
+
+	unsigned int speech_;
+	unsigned int action_;
+	unsigned int strength_;
+
+	const std::string value_;
 };
+
+
 
 class Dictionary
 {
