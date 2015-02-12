70faa0376c6400e09ef90e54a36defabef3d952e
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-02-12 22:39:13 +0000

    make it compile with the new enums

--- src/cmd/dictionary.cpp
+++ src/cmd/dictionary.cpp
@@ -5,12 +5,12 @@
 Dictionary::Dictionary()
 {
 	dictionary_[ "attack" ] = new Entry( std::string( "attack" ), Verb, Attack, Strong );
-	dictionary_[ "move"   ] = new Entry( std::string( "move" ), Verb, Neutral, Neutral );
+	dictionary_[ "move"   ] = new Entry( std::string( "move" ), Verb, Neutral, Medium );
 	dictionary_[ "defend" ] = new Entry( std::string( "defend" ), Verb, Defend, Strong );
-	dictionary_[ "support"] = new Entry( std::string( "support" ), Verb, Attack, Neutral );
-	dictionary_[ "advance"] = new Entry( std::string( "advance" ), Verb, Attack, Neutral );
+	dictionary_[ "support"] = new Entry( std::string( "support" ), Verb, Attack, Medium );
+	dictionary_[ "advance"] = new Entry( std::string( "advance" ), Verb, Attack, Medium );
 	dictionary_[ "occupy" ] = new Entry( std::string( "occupy" ), Verb, Neutral, Weak );
-	dictionary_[ "hold"   ] = new Entry( std::string( "hold" ), Verb, Defend, Neutral );
+	dictionary_[ "hold"   ] = new Entry( std::string( "hold" ), Verb, Defend, Medium );
 }
 
 Entry Dictionary::Get( std::string )
