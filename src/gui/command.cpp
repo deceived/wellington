c96e67727fe223874375876d218fb46afdf7f771
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-03-05 20:58:42 +0000

    remove unintended command

--- src/gui/command.cpp
+++
@@ -1,8 +0,0 @@
-void StdScrMapController::DisplayCommand()
-{
-	logger.Severity( severity_level::info, __PRETTY_FUNCTION__ );
-
-	line_ptr prompt = command_->GetPrompt();
-	view_->ClearLine( 40, 0, 80 );
-	view_->Put( 40, 0, prompt );
-}
