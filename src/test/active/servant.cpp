6aac5463592e16993402980fb6415aab4b097b1f
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-03-07 14:10:44 +0000

    cut servant down to minimum. lets try how the move order might be implemented.

--- src/test/active/servant.cpp
+++ src/test/active/servant.cpp
@@ -1,8 +1,7 @@
 #include "servant.hpp"
 
 
-Servant::Servant( std::size_t size )
-	: size_( size )
+Servant::Servant()
 {
 }
 
@@ -10,35 +9,7 @@ Servant::~Servant()
 {
 }
 
-void Servant::Put( const Message& message )
-{
-	messages_.push_back( message );
-}
-
-Message Servant::Get()
-{
-	return Message();
-}
-
-bool Servant::Empty() const
-{
-	return messages_.empty();
-}
-
-bool Servant::Full() const
-{
-	return messages_.size() < size_ ? false : true;
-}
-
 void Servant::Move()
-{
-}
-
-void Servant::Fight()
-{
-}
-
-void Servant::Command()
 {
 }
 
--- src/test/active/servant.hpp
+++ src/test/active/servant.hpp
@@ -1,34 +1,17 @@
 #ifndef SERVANT_HPP
 #define SERVANT_HPP
 
-#include <cstddef>
-#include <list>
-
-#include "message.hpp"
-
 class Servant
 {
 
 public:
 
-	Servant( std::size_t size );
+	Servant();
 	~Servant();
 
-	void Put( const Message& message );
-	Message Get();
-
-	bool Empty() const;
-	bool Full() const;
-
 	void Move();
-	void Fight();
-	void Command();
 
 private:
-
-	std::size_t	size_;
-
-	std::list< Message > messages_;
 
 };
 
