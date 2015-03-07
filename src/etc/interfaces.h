3726d7eae41486cb87ba3e331dc88eb3be6854db
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-03-07 17:49:40 +0000

    move some typedefs to the interfaces header.

--- src/etc/interfaces.h
+++ src/etc/interfaces.h
@@ -1,6 +1,8 @@
 #ifndef INTERFACES_HPP
 #define INTERFACES_HPP
 
+#include <string>
+
 #include <boost/smart_ptr.hpp>
 #include <boost/date_time/posix_time/posix_time.hpp>
 
@@ -11,11 +13,15 @@ typedef boost::shared_ptr<Actor>    ActorPtr;
 typedef boost::shared_ptr<ActorComponent>   ActorComponentPtr;
 typedef boost::shared_ptr< boost::posix_time::time_duration >  timeout_ptr;
 
-//typedef unsigned long ActorId;
-//typedef unsigned long ActorComponentId;
-
 typedef std::size_t ActorId;
 typedef std::size_t ComponentId;
+
+class Command;
+class Map;
+
+typedef boost::shared_ptr< std::string >	line_ptr;
+typedef boost::shared_ptr< Command > cmd_ptr;
+typedef boost::shared_ptr< Map > map_ptr;
 
 #endif
 
