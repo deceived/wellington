a18578e9bf0f364f26506ba2ec2743a830d267be
John G Ratcliffe <john@firecrest.eclipse.co.uk>
2015-03-13 21:53:28 +0000

    start working with a scheduler

--- src/test/active/main.cpp
+++ src/test/active/main.cpp
@@ -4,8 +4,10 @@
 int main( int argc, char** argv )
 {
 
-//	const std::size_t max_threads = 1;
-//	const std::size_t max_requests = 3;
+	const std::size_t max_threads = 1;
+	const std::size_t max_requests = 3;
+
+	priority_scheduler scheduler( max_threads, max_requests );
 
 	return 0;
 }
